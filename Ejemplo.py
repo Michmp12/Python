2a2b01934d0ed43a2b71f80cc6e0fe43bd28d3f8
MichMP <158522214+Michmp12@users.noreply.github.com>
2024-02-22 17:38:08 -0500

    Ejercicio 1 de Python

---
+++ Ejemplo.py
@@ -0,0 +1,6 @@
+nota1Flt = float(input('Ingrese la nota 1 -> '))
+nota2Flt = float(input('Ingrese la nota 2 -> '))
+nota3Flt = float(input('Ingrese la nota 3 -> '))
+
+definitivaFlt = (nota1Flt + nota2Flt + nota3Flt) / 3
+print('su nota definitiva es: ', definitivaFlt)
